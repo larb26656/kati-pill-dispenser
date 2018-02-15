a17abe2ed33a51736cff66162313e7503fc86b82
Errortime <larb26656@gmail.com>
2018-02-15 10:49:18 +0700

    add serial service.

---
+++ serial_service.py
@@ -0,0 +1,37 @@
+"""import serial
+import time
+
+from click._compat import raw_input
+
+arduino = serial.Serial('COM3', 9600)
+arduino.write('ssss'.encode())
+while True:
+	data = arduino.readline()[:-2] #the last bit gets rid of the new-line 
+	if data:
+		print(data)
+	time.sleep(1) """
+from time import sleep
+import serial
+from click._compat import raw_input
+
+ser = serial.Serial('COM3', 9600) # Establish the connection on a specific port
+
+def moving_step_motor(slot_num,num_of_dispenser):
+     sleep(1)
+     json_text = "{'type':'pill_dispenser','slot_num':"+str(slot_num)+",'num_of_dispenser':"+str(num_of_dispenser)+"}"
+     ser.write(json_text.encode())
+     ser.flush()
+     print(ser.readline()[:-2].decode())
+
+
+def get_current_time():
+     sleep(1)
+     json_text = "{'type':'current_time'}"
+     ser.write(json_text.encode())
+     ser.flush()
+     print(ser.readline()[:-2].decode())
+
+#moving_step_motor(1,1)
+moving_step_motor(2,3)
+#moving_step_motor(2,3)
+get_current_time()
